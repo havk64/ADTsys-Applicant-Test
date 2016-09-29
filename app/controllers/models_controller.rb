4cbd8c0587048ca6d182fb4f8787f8d359612834
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-09-29 11:38:52 -0300

    Refactoring controller to changes on Service Object

--- app/controllers/models_controller.rb
+++ app/controllers/models_controller.rb
@@ -1,13 +1,16 @@
 class ModelsController < ApplicationController
   def index
-    make = Make.where(webmotors_id: params[:webmotors_make_id])[0]
-    @models = Model.where(make_id: make.id)
+    @make = set_make
+    @models = Model.where(make_id: @make.id)
     update_models
   end
 
   private
     def update_models
-      @check = UpdateModels.new
+      @check = UpdateModels.new(@make, params[:webmotors_make_id])
       @check.update
+    end
+    def set_make
+      @make = Make.where(webmotors_id: params[:webmotors_make_id])[0]
     end
 end
