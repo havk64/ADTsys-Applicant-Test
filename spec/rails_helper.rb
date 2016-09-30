6ac0661c9e26e385b43f4270a217278b595a2893
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-09-30 14:41:56 -0300

    Add shoulda-matchers configuration

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -55,3 +55,12 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    # Define a test framework:
+    with.test_framework :rspec
+    # Define libraries:
+    with.library :rails
+  end
+end
