5c3b0a4da86257ac6b4d5182e65da5a6ba06396d
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-09-30 18:28:39 -0300

    Fixing port number for vagrant instance

--- config/puma.rb
+++ config/puma.rb
@@ -3,7 +3,7 @@ workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
 threads threads_count, threads_count
 
-port        ENV.fetch("PORT") { 5000 }
+port        ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { "development" }
 
 preload_app!
