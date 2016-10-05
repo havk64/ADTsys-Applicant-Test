32a2771c543498e9e8ff5d1845dcc8ebfe5325cd
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-10-05 19:18:14 -0300

    Prepare puma conf to reverse proxy

--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,5 @@
+app_dir = File.expand_path("../..", __FILE__)
+
 workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
 threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }.to_i
@@ -5,6 +7,10 @@ threads threads_count, threads_count
 
 port        ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { "development" }
+
+bind "unix://#{app_dir}/tmp/sockets/puma.sock"
+pidfile "#{app_dir}/tmp/pids/puma.pid"
+state_path "#{app_dir}/tmp/pids/puma.state"
 
 preload_app!
 
